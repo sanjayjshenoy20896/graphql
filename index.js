16fe64aefa9f074d1034225c85ed4d96bb2ca48b
unknown <sanjayjshenoy20896@gmail.com>
2025-12-12 11:07:23 +0530

    resolvers

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone'
 
 //types
-import { typeDefs } from './schema';
+import { typeDefs } from './schema.js';
+import _db from './_db.js';
+
+const resolvers = {
+    Query:{
+        reviews:()=>{
+            return _db.reviews
+        },
+        games:()=>{
+            return _db.games;
+        },
+        authors:()=>{
+            return _db.authors;
+        }
+    }
+}
 
 //server setup 
 const server = new ApolloServer({
-  typeDefs
-  // resol vers
+  typeDefs,
+  resolvers
 })
 
 const { url } = await startStandaloneServer(server, {
