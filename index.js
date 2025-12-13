5402924a900b96fff4fbaae88f7bc859cbdca956
unknown <sanjayjshenoy20896@gmail.com>
2025-12-13 07:22:25 +0530

    update game

--- index.js
+++ index.js
@@ -45,6 +45,29 @@ const resolvers = {
       return db.reviews.filter((r) => r.author_id === parent.id)
     }
   },
+  Mutation:{
+    deleteGame:(_,args)=>{
+      db.games = db.games.filter((game)=> game.id!=args.id);
+      return db.games;
+    },
+    addGame:(_,args)=>{
+      let game = {
+        ...args.game,
+        id:Math.floor(Math.random()*10000).toString()
+      }
+      db.games.push(game)
+      return game
+    },
+    updateGame:(_,args)=>{
+      db.games = db.games.map((game)=>{
+        if(game.id === args.id ){
+          return {...g,...args.edits}
+        }
+        return game;
+      })
+      return db.games.find((g) => g.id === args.id)
+    }
+  }
 }
 
 //server setup 
--- schema.js
+++ schema.js
@@ -26,6 +26,21 @@ export const typeDefs = `#graphql
     authors: [Author]
     author(id:ID!):Author
   }
+  type Mutation {
+    deleteGame(id:ID!):[Game],
+    addGame(game:AddGameInput):Game,
+    updateGame(id:ID!,edits:EditGameInput!):Game
+  },
+  input AddGameInput{
+    title:String,
+    platform:[String!]!
+  }
+  input EditGameInput{
+    title:String,
+    platform:[String!]!
+  }
+  
+
 `
 
 // int ,float,string,boolean,ID
